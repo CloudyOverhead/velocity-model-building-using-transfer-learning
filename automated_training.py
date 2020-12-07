4becdc3b4f484aadd48f002598a918f43df24eaa
CloudyOverhead <jerome.simon@ete.inrs.ca>
2020-12-07 03:17:16 -0500

    Fix automated training
    Use all hyperparameters in the same `Hyperparameters` object. Make 
    script more general by fetching hyperparameters from `architecture` 
    module.

--- automated_training.py
+++ automated_training.py
@@ -1,29 +1,23 @@
 # -*- coding: utf-8 -*-
 """Launch hyperoptimization and chain training stages."""
 
-from GeoFlow.RCNN2D import Hyperparameters
-from GeoFlow import Dataset2Dtest
+from argparse import ArgumentParser
+
 from GeoFlow.AutomatedTraining.AutomatedTraining import optimize
 
+from deep_learning_velocity_estimation import datasets, architecture
+from deep_learning_velocity_estimation.architecture import RCNN2D
 
+
+parser = ArgumentParser()
+parser.add_argument("--params")
+parser.add_argument("--dataset")
+args = parser.parse_args()
+
+args.params = getattr(architecture, args.params)()
+args.dataset = getattr(datasets, args.dataset)()
 optimize(
-    params=Hyperparameters(),
-    case=Dataset2Dtest(),
-    epochs=(100, 100, 50),
-    steps=20,
-    lr=.0002,
-    beta_1=.9,
-    beta_2=.98,
-    eps=1e-5,
-    batchsize=4,
-    loss_ref=(.5, .0, .0),
-    loss_vrms=(.5, .7, .0),
-    loss_vint=(.0, .3, 1.),
-    loss_vdepth=(.0, .0, .0),
-    nmodel=1,
-    ngpu=2,
-    noise=0,
-    plot=0,
-    no_weights=False,
-    restore_from=None,
+    architecture=RCNN2D,
+    params=args.params,
+    dataset=args.dataset,
 )
---
+++ deep_learning_velocity_estimation/architecture.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""Build the neural network for predicting v_p in 2D and in depth."""
+
+from GeoFlow.RCNN2D import RCNN2D, Hyperparameters
+
+
+class RCNN2D(RCNN2D):
+    pass
+
+
+class Hyperparameters(Hyperparameters):
+    pass
