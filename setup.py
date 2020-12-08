3e1fd7be8d9ffc624410f8a1c79d408cbe719d05
CloudyOverhead <jerome.simon@ete.inrs.ca>
2020-12-08 06:10:49 -0500

    Update requirement for `Deep_2D_velocity`

--- setup.py
+++ setup.py
@@ -4,19 +4,26 @@ from setuptools import setup, find_packages
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
-GITHUB_REQUIREMENT = ("{name} @ git+git://git.github.com/{author}/{name}.git"
-                      "@{version}#egg={name}")
-REQUIREMENTS = [GITHUB_REQUIREMENT.format(author="gfabieno",
-                                          name="SeisCL",
-                                          version="eef941d4e31b5fa0dc7823e491e"
-                                                  "0575ad1e1f423"),
-                GITHUB_REQUIREMENT.format(author="gfabieno",
-                                          name="ModelGenerator",
-                                          version="a01cbb93d67a6c859ea6ac67ea5"
-                                                  "5361208b66e7c"),
-                GITHUB_REQUIREMENT.format(author="gfabieno",
-                                          name="GeoFlow",
-                                          version="")]
+GITHUB_REQUIREMENT = (
+    "{name} @ git+https://github.com/{author}/{name}.git@{version}"
+)
+REQUIREMENTS = [
+    GITHUB_REQUIREMENT.format(
+        author="gfabieno",
+        name="SeisCL",
+        version="eef941d4e31b5fa0dc7823e491e0575ad1e1f423",
+    ),
+    GITHUB_REQUIREMENT.format(
+        author="gfabieno",
+        name="ModelGenerator",
+        version="a01cbb93d67a6c859ea6ac67ea55361208b66e7c",
+    ),
+    GITHUB_REQUIREMENT.format(
+        author="gfabieno",
+        name="Deep_2D_velocity",
+        version="f4b0031cae1e3ae21e5eb881af7c7ef22e4f56f0",
+    ),
+]
 
 setup(
     name="deep-learning-seismic-velocity-estimation",
