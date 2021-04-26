6a9aa0e7a45ba8bb6e5c144c58530b233b9a211b
CloudyOverhead <jerome.simon@geolearn.ca>
2021-04-26 10:33:15 -0400

    Use new main in automated training

--- core/automated_training.py
+++ core/automated_training.py
@@ -1,64 +1,17 @@
 # -*- coding: utf-8 -*-
 """Launch hyperoptimization and chain training stages."""
 
-from argparse import ArgumentParser
+from GeoFlow.AutomatedTraining.AutomatedTraining import optimize
 
-from AutomatedTraining.AutomatedTraining import optimize, int_or_list
-
-from core import datasets, architecture
+from core.__main__ import parse_args
 from core.architecture import RCNN2D
 
 
-parser = ArgumentParser()
-parser.add_argument(
-    "--params",
-    type=str,
-    default="Hyperparameters",
-    help="Name of hyperparameters from `RCNN2D` to use.",
-)
-parser.add_argument(
-    "--dataset",
-    type=str,
-    default="Dataset1Dsmall",
-    help="Name of dataset from `DefinedDataset` to use.",
-)
-parser.add_argument(
-    "--gpus",
-    type=int_or_list,
-    default=None,
-    help=(
-        "Either the quantity of GPUs or a list of GPU IDs to use in data "
-        "creation, training and inference. Use a string representation "
-        "for lists of GPU IDs, e.g. `'[0, 1]'` or `[0,1]`. By default, "
-        "use all available GPUs."
-    ),
-)
-parser.add_argument(
-    "--noise",
-    action='store_true',
-    help="Add statics, noise and random gain to input data.",
-)
-parser.add_argument(
-    "--debug",
-    action='store_true',
-    help="Generate a small dataset of 5 examples.",
-)
-parser.add_argument(
-    "--eager",
-    action='store_true',
-    help="Run the Keras model eagerly, for debugging.",
-)
-args, config = parser.parse_known_args()
-config = {
-    name[2:]: eval(value) for name, value in zip(config[::2], config[1::2])
-}
-
-args.dataset = getattr(datasets, args.dataset)(args.noise)
-args.params = getattr(architecture, args.params)()
+args = parse_args()
 
 if args.debug:
-    config["epochs"] = 1
-    config["steps_per_epoch"] = 5
+    args.params["epochs"] = 1
+    args.params["steps_per_epoch"] = 5
 
 optimize(
     nn=RCNN2D,
@@ -67,5 +20,5 @@ optimize(
     gpus=args.gpus,
     debug=args.debug,
     eager=args.eager,
-    **config,
+    **args.params,
 )
